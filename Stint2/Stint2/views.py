77d6e584664ecd192582448dc3ddd763ccf10604
chanmadeleine <madeleine.chan@stud.leuphana.de>
2018-09-11 22:43:14 +0800

    Stint2

--- Stint2/Stint2/views.py
+++ Stint2/Stint2/views.py
@@ -35,3 +35,10 @@ def about():
         year=datetime.now().year,
         message='Your application description page.'
     )
+
+import os 
+from flask import send_from_directory     
+
+@app.route('/favicon.ico') 
+def favicon(): 
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
